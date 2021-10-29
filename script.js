f83b636a5d7ed6ba2ae166361f6860c45c11f27c
csetowhid <towhidhasang1@gmail.com>
2021-10-29 20:58:23 +0600

    Inner Html

---
+++ script.js
@@ -0,0 +1,7 @@
+document.write('Hellow World! </br>');
+document.write(11+12);
+console.log("I Will Go");
+
+document.getElementById('heading');
+document.getElementById('heading').innerHTML= 'After Change';
+
