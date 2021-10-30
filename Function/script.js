d0e85ee0e576538f4de435cafc4f7ba8a5298f68
csetowhid <towhidhasang1@gmail.com>
2021-10-30 09:00:12 +0600

    Function

---
+++ Function/script.js
@@ -0,0 +1,11 @@
+// function test(){
+//     document.getElementById('background').style.backgroundColor = 'blue';
+// }
+
+const something = 50;
+function test(nothing,nothing2){
+    console.log(nothing);
+    console.log(nothing2);
+}
+// test(something);
+test('We are sending a value','Another argument');
