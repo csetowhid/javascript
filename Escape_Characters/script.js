2c262ad9c1fa9a9a17aa246cb08b1b1a398b8395
csetowhid <towhidhasang1@gmail.com>
2021-10-30 09:50:38 +0600

    special character

---
+++ Escape_Characters/script.js
@@ -0,0 +1,8 @@
+const str = "I don\'t want to go there. And she said \"he has a pen\"";
+const str2 = "This is backword \\ slash";
+console.log(str2);
+
+const poem = 'First Line \nSecond Line \nThird Line'
+const poem2 = 'First Line \tSecond Line'
+
+console.log(poem2);
