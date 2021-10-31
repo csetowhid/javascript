8a5ed7f6603030657454669ecdbeb120df969eea
csetowhid <towhidhasang1@gmail.com>
2021-10-31 11:14:23 +0600

    Number Method

---
+++ Number/script.js
@@ -0,0 +1,19 @@
+console.log(Number.MAX_VALUE);
+console.log(1.7976931348623159e+308);
+
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_SAFE_INTEGER);
+
+
+
+//parse
+
+const str = '2020';
+const num = parseInt(str);
+console.log(typeof num);
+
+
+const bin = '101';
+const num2 = parseInt(bin, 2);
+console.log(num2);
