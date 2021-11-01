d09634bfb905aece688cbf82f8dfbf8e0af931b8
csetowhid <towhidhasang1@gmail.com>
2021-11-01 11:25:45 +0600

    array method

--- Array/script.js
+++ Array/script.js
@@ -4,5 +4,27 @@ const person = [
     'Bangladesh'
 ]
 
-console.log(person[0]);
+// console.log(person[0]);
 // console.log(typeof person);
+
+const arr = [1,2,3,4,5,6];
+arr[0] = 'one';
+arr[1] = 'two';
+// console.log(arr);
+
+
+const arr2 = [1,2,3];
+arr2.push(4);
+// arr2.pop();
+arr2.unshift(0);
+arr2.shift();
+// console.log(arr2);
+
+const arr3 = [1,2,3,4,5];
+const arr4 = [6,7,8,9,10];
+// const arr5 = arr3.concat(arr4);
+const arr5 = arr3.slice(0,3);
+const arr6 = arr3.splice(0,3,'one','two','three');
+console.log(arr5);
+console.log(arr6);
+console.log(arr);
