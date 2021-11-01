7afb87396e5c98e9bae63ad6e08bf7d0e2d0d382
csetowhid <towhidhasang1@gmail.com>
2021-11-01 12:00:23 +0600

    Array Methods End

--- Array/script.js
+++ Array/script.js
@@ -28,3 +28,19 @@ const arr6 = arr3.splice(0,3,'one','two','three');
 console.log(arr5);
 console.log(arr6);
 console.log(arr);
+
+
+
+const arr7 = [1,2,3,4,5,2];
+// console.log(arr7.indexOf(2));
+// console.log(arr7.lastIndexOf(2));
+
+const arr8 = [1,2,3,4,5];
+console.log(arr8.reverse());
+
+const str = ['one','two','three','four'];
+const str2 = [10,30,5,333,80];
+str2.sort(function(a,b){
+    return b - a;
+})
+console.log(str2);
