9c3377711cbcb95866fc6f32a8fa8c37811c9ea7
csetowhid <towhidhasang1@gmail.com>
2021-11-01 10:32:37 +0600

    switch

---
+++ switch/script.js
@@ -0,0 +1,31 @@
+const day = prompt('The Day of Reservation');
+
+switch(day){
+    case 'Saturday':
+        console.log('10 am - 7 Pm');
+        break;
+    case 'Saturday':
+        console.log('10 am - 7 Pm');
+        break;
+    case 'Sunday':
+        console.log('Closed');
+        break;
+    case 'Monday':
+        console.log('11 am - 8 Pm');
+        break;
+    case 'Tuesday':
+        console.log('12 pm - 9 Pm');
+        break;
+    case 'Wednesday':
+        console.log('10 am - 8 Pm');
+        break;
+    case 'Thursday':
+        console.log('2 pm - 8 Pm');
+        break;
+    case 'Friday':
+        console.log('5 am - 11 Pm');
+        break;
+
+    default:
+        console.log('This Is Not A Day');
+}
